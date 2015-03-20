46647750f08b2c724bdf8a7715bbb204f1a1ead7
Steve Kenworthy <steveyken@gmail.com>
2015-03-20 17:05:11 +0800

    Added a log formatter

--- logger.js
+++ logger.js
@@ -1,6 +1,16 @@
+var util = require("util");
 var winston = require("winston");
 var config = require("./config.js");
-var logger = winston.loggers.add("socketio", config.winston);
-logger.exitOnError = false;
+var options = config.winston;
 
+// add our custom formatter for file logs
+if (options.file) {
+  options.file['formatter'] = function(options) {
+    return 'time="' + (new Date().toISOString()) +'" level="'+ options.level.toUpperCase() +'" message="'+ (undefined !== options.message ? options.message : '') + '" meta="' +
+      (options.meta && Object.keys(options.meta).length ? JSON.stringify(options.meta) : '' ) + '"';
+  }
+}
+
+var logger = winston.loggers.add("socketio", options);
+logger.exitOnError = false;
 module.exports = logger;

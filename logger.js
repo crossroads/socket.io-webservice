651ef46361454e1cc7ad76526f7c3ac37c939c95
Steve Kenworthy <steveyken@gmail.com>
2015-04-10 11:58:37 +0800

    Improved log format

--- logger.js
+++ logger.js
@@ -6,8 +6,23 @@ var options = config.winston;
 // add our custom formatter for file logs
 if (options.file) {
   options.file['formatter'] = function(options) {
-    return 'time="' + (new Date().toISOString()) +'" level="'+ options.level.toUpperCase() +'" message="'+ (undefined !== options.message ? options.message : '') + '" meta="' +
-      (options.meta && Object.keys(options.meta).length ? JSON.stringify(options.meta) : '' ) + '"';
+    var text = 'time="' + (new Date().toISOString()) +'" level="'+ options.level.toUpperCase() +'" message="'+ (undefined !== options.message ? options.message : '') + '"';
+
+    if (options.meta) {
+      if (typeof options.meta === 'object') {
+        for (key in options.meta) {
+          var str = options.meta[key];
+          if (typeof str === 'object') {
+            str = JSON.stringify(str);
+          }
+          str = str.toString().replace(/"/g, "'");
+          text += ' ' + key + '="' + str + '"';
+        }
+      } else {
+        text += ' meta="' + options.meta.replace(/"/g, "'") + '"';
+      }
+    }
+    return text;
   }
 }
 

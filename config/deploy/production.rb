91a9b22ec7ca7ea79705466e8eea60aba97d30d8
Steve Kenworthy <steveyken@gmail.com>
2016-09-28 11:04:06 +0800

    Updated port for capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
-server 'socket.goodcity.hk', user: 'deployer', roles: %w{web}, primary: true
+server 'socket.goodcity.hk:62422', user: 'deployer', roles: %w{web}, primary: true
 server 'goodcity-production.cloudapp.net:62423', user: 'deployer', roles: %w{web}
 set :branch, :live

21eeb079f12f27ad96c11a63992a0bfcce6ec1a5
namrataukirde <namrata.ukirde@kiprosh.com>
2017-07-27 10:28:48 +0530

    GCW-1656 Cannot read property statusCode of undefined

--- app.js
+++ app.js
@@ -171,7 +171,7 @@ for (var siteName in config.sites) {
     var query = url.parse(socket.request.url, true).query;
     var headers = {"Authorization": site.authScheme + " " + query.token};
     httpClient.put(userUpdateUrl, {headers:headers, json:userParams}, function(error, res, body) {
-      if (error || res.statusCode !== 200) {
+      if (error || (res.statusCode && res.statusCode !== 200)) {
         logger.error({"category":"User update error","site":nsp.name,"message":error || body,"status code":res.statusCode,"socketId":socket.id,"deviceId":socket.deviceId,"userRoom":userRoom});
       }
       else {
@@ -190,10 +190,10 @@ for (var siteName in config.sites) {
       if (error) {
         logger.error({"category":"authentication","site":nsp.name,"message":"Client auth error: " + error});
         return next(new Error("Auth error: " + error));
-      } else if (res.statusCode === 401) {
+      } else if (res.statusCode && res.statusCode === 401) {
         logger.error({"category":"authentication","site":nsp.name,"message":"Client auth failed"});
         return next(new Error("Authentication failed"));
-      } else if (res.statusCode !== 200) {
+      } else if (res.statusCode && res.statusCode !== 200) {
         logger.error({"category":"authentication","site":nsp.name,"message":"Auth " + res.statusCode + " error"});
         return next(new Error("Auth " + res.statusCode + " error"));
       } else if (site.userRoomEnabled && !data.some(function(r) { return nsp.isUserRoom(r); })) {

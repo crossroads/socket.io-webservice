9fcf74afd654c1dbed4f078b8713d9d7ddadb874
Steve Kenworthy <steveyken@gmail.com>
2017-09-28 14:41:25 +0800

    Removed 2nd server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,2 @@
 server 'socket.goodcity.hk:62422', user: 'deployer', roles: %w{web}, primary: true
-server 'goodcity-production.cloudapp.net:62423', user: 'deployer', roles: %w{web}
 set :branch, :live
